a758efa28d88cc631f1036ecfc560a51caef0444
Nicholas Patel <np4795@yahoo.com>
2020-02-11 22:38:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
+
 def unsafe?(speed)
-
+  if speed > 60 
+		true
+	elsif 
+		speed < 40
+		true
+	else
+		false
+		end
 end
-
+unsafe? 79
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
+not_safe? 80
+ 
